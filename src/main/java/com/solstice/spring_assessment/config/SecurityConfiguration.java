8ed8059243a598f8fbe7d1440ba17d6716fd17ad
Alex <alexdan93@hotmail.com>
2019-01-11 16:29:27 -0300

    added security configuration

--- src/main/java/com/solstice/spring_assessment/config/SecurityConfiguration.java
+++ src/main/java/com/solstice/spring_assessment/config/SecurityConfiguration.java
@@ -1,4 +1,37 @@
 package com.solstice.spring_assessment.config;
 
-public class SecurityConfiguration {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Value("${application.environment}")
+    private String environment;
+
+    @Autowired
+    protected void configureUsers(AuthenticationManagerBuilder auth) throws Exception {
+        if ("qa".equals(environment)) {
+            auth.inMemoryAuthentication()
+                    .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                    .withUser("qaUser").password("32#04").roles("ADMIN");
+        } else if ("prod".equals(environment)) {
+            auth.inMemoryAuthentication()
+                    .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                    .withUser("prodUser").password("445#2").roles("ADMIN");
+        }
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().fullyAuthenticated().and().httpBasic();
+    }
+
 }
